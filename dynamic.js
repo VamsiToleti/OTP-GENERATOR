7f66858db74af6f90cd51f8cbde9fffb072a216a
VamsiToleti <155719253+VamsiToleti@users.noreply.github.com>
2025-05-07 16:14:24 +0530

    Add files via upload

---
+++ dynamic.js
@@ -0,0 +1,13 @@
+
+let ip=document.querySelectorAll('input');
+for(let i=0;i<ip.length;i++){
+    ip[i].style.width="24px";
+}
+let btn=document.querySelector('button');
+btn.addEventListener('click',function(){
+    let otp=Math.ceil(Math.random()*9999);
+    otp=String(otp);
+    for(let i=0;i<otp.length;i++){
+        ip[i].value=otp[i];
+    }
+})
